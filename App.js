f92252b946aacb1808be68c4796b357de458ab9b
Sisi Small <smallsianah@gmail.com>
2023-08-09 23:03:34 -0400

    created a car fidget app

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { Asset } from 'expo-asset';
 
 export default function App() {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  // List of free-to-use car images (replace with actual image paths)
+  const carImages = [
+    require('./path_to_car1.jpg'),
+    require('./path_to_car2.jpg'),
+    require('./path_to_car3.jpg'),
+    // ... add more images as needed
+  ];
+
+  const handleImageTap = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % carImages.length);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableOpacity style={styles.container} onPress={handleImageTap}>
+      <Image source={carImages[currentImageIndex]} style={styles.carImage} resizeMode="contain" />
+    </TouchableOpacity>
   );
 }
 
@@ -17,4 +31,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  carImage: {
+    width: '100%',
+    height: '100%',
+  },
 });
+
